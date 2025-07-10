735019e1f74f5d3c8d7b1f691782f9fce002b854
niklasgolf <niklas.herrloff@gmail.com>
2025-07-10 16:27:59 +0200

    test commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
+      <h1>Welcome to my Home Page</h1>
       <Link href="/notmember">Go to Not Member Page</Link>
       <br />
       <Link href="/home">Go to Home Page</Link>
